04ef00712145c2ce2a0d81a7770ef3f4ff7d8741
joe-re <joe.tialtngo@gmail.com>
2017-02-05 06:26:03 +0900

    delete unused file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-import 'babel-polyfill';
-import getReleases from './getReleases';
-
-getReleases(process.argv[2]).then(releases => {
-  console.log(releases);
-}).catch(error => console.log(error));
